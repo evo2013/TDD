66cb9985d113deda46c365330b2a1c873e9a3120
evo2013 <CVMailer_2011@yahoo.com>
2015-11-24 22:24:19 -0800

    difference-of-squares challenge

---
+++ difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,20 @@
+class Squares
+  attr_writer :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def square_of_sums
+    sum = (1..@number).reduce(:+)
+    sum ** 2
+  end
+
+  def sum_of_squares
+    squares = (1..@number).inject { |sum, n| sum + (n ** 2) }
+  end
+
+  def difference
+    square_of_sums - sum_of_squares
+  end
+end
