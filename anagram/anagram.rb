24dd475483a4cb0092f13ede72072bfafa496823
evo2013 <CVMailer_2011@yahoo.com>
2015-12-04 22:53:09 -0800

    anagram challenge

---
+++ anagram/anagram.rb
@@ -0,0 +1,19 @@
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def match(args)
+    @result = []
+    args.each do |argument|
+      next if argument.downcase == @word.downcase
+      @result << argument if argument.downcase.chars.sort.join == @word.downcase.chars.sort.join
+    end
+    @result
+  end
+
+  def sort
+    @result = @result.sort
+  end
+
+end
