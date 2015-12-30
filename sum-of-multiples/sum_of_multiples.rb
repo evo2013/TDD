6a74a645ea5fcf05a6230fb7c03669ea0e6a039e
evo2013 <CVMailer_2011@yahoo.com>
2015-12-29 23:07:43 -0800

    style guide refinements

--- sum-of-multiples/sum_of_multiples.rb
+++ sum-of-multiples/sum_of_multiples.rb
@@ -1,20 +1,18 @@
 class SumOfMultiples
-
   def initialize(*args)
     @args = args
     @multiples = []
   end
 
   def self.to(input)
-    @multiples =  (0...input).select { |num| num % 3 == 0 || num % 5 == 0}
+    @multiples = (0...input).select { |num| num % 3 == 0 || num % 5 == 0 }
     @multiples.reduce(:+)
   end
 
   def to(input)
     @args.each do |arg|
-      @multiples <<  (0...input).select { |num| num % arg == 0 }
+      @multiples << (0...input).select { |num| num % arg == 0 }
     end
     @multiples.flatten!.uniq!.reduce(:+)
   end
 end
-
