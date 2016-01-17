bf0699680cec64f2de730cb4f40eec8804c5decd
evo2013 <CVMailer_2011@yahoo.com>
2016-01-17 01:39:09 -0800

    matrix challenge

---
+++ matrix/matrix.rb
@@ -0,0 +1,31 @@
+class Matrix
+  def initialize(rows_columns)
+    rc = rows_columns.split("\n")
+    @rows_columns = []
+    rc.each do |item|
+      @rows_columns << item.split(" ").map(&:to_i)
+    end
+
+    @rows_columns
+  end
+
+  def rows
+    @rows_columns
+  end
+
+  def columns
+    counter = 0
+    single_column = []
+    all_columns = []
+    while counter < @rows_columns[0].length
+      @rows_columns.each do |list|
+        single_column << list[counter]
+      end
+      all_columns << single_column
+      counter += 1
+      single_column = []
+    end
+
+    all_columns
+  end
+end
